304a5b8c7a6320e76ccd269fb0ffc8ca23b3a6f1
Nugie kurniawan <nugiebinhar@gmail.com>
2025-08-16 12:14:21 +0700

    16 agustus 2025/array dasar

---
+++ 16Agustus2025/array/script.js
@@ -0,0 +1,16 @@
+// belajar array
+// array di javascript merupakan object khusus yang dapat menyimpan banyak nilai sekaligus
+
+// membuat array
+let mahasiswa = ["Nugie kurniawan","Rakha sagraha","Rizal dadang hidayatulloh"]
+
+// mengetahui panjang array menggunakan method length
+console.log(mahasiswa.length)
+
+// mencetak satu data array
+console.log(mahasiswa[0])
+
+// mencetak seluruh data mahasiswa
+for(let i = 0; i<mahasiswa.length;i++){
+    console.log(mahasiswa[i])
+}
