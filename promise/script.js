d03aa33ed9346a793e6ffab20b9378fff3c06b85
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-15 13:02:45 +0700

    latihan promise dan ajax

--- promise/script.js
+++ promise/script.js
@@ -6,15 +6,35 @@
 -finally -> setelah fullfiled atau rejected selesai akan di jalankan finally -> finally
 */
 
-let berhasil = false
-const janjiSetia = new Promise((resolve,reject)=>{
-    if(berhasil){
-        resolve("Ini berhasil")
-    }else{
-        reject("Ini tidak berhasil")
-    }
-})
-janjiSetia
-    .then((response=>console.log(response)))
-    .catch((response=>console.log(response)))
+// let berhasil = false
+// const janjiSetia = new Promise((resolve,reject)=>{
+//     if(berhasil){
+//         resolve("Ini berhasil")
+//     }else{
+//         reject("Ini tidak berhasil")
+//     }
+// })
+// janjiSetia
+//     .then((response=>console.log(response)))
+//     .catch((response=>console.log(response)))
     
+let getData = function(link){
+    return new Promise((resolve,reject)=>{
+        const xhr = new XMLHttpRequest()
+        xhr.onload = function(){
+            if(xhr.status == 200){
+                resolve(JSON.parse(xhr.response))
+            }else{
+                reject("GAGAL AMBIL DATA YA ANJING")
+            }
+        }
+        xhr.open("GET",link)
+        xhr.send()
+    })
+}
+getData("https://bukuacak-9bdcb4ef2605.herokuapp.com/api/v1/book?page=1&year=2023&genre=Self-Improvement&keyword=Berani").then(({books})=>{
+    let teks = books.map(el=>`<li>${el.title}</li>`).join("")
+    document.body.innerHTML = `<ol>
+    ${teks}
+    </ol>`
+}).catch((reponse=>console.log(response)))
